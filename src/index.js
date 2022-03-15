cf245c6102e1ea40dca98c30bf6b2fed5e5ee508
2heeesss <hohooodo@gmail.com>
2022-03-15 22:50:15 +0900

    [Feat] lint test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
+import Test from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
